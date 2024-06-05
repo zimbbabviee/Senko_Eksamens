345a8a1606915028aa4ca83cfce4485e966c9b5b
Jevgenija <evgenijasenjko18@gmail.com>
2024-06-05 21:08:38 +0300

    klase Tests un funkcija pildit_testu

---
+++ tests.py
@@ -0,0 +1,24 @@
+﻿class Tests:
+    def __init__(self, jautajumi):
+        self.jautajumi = jautajumi
+        
+    def pildit_testu(self):  
+        for i, q in enumerate(self.jautajumi, 1):
+            print("--------------------------------------------------------------------------------")
+            print(f"\nJautajums {i}: {q['jautajums']}")
+            for atbilde in q['atbildes']:
+                print(atbilde)
+                
+jautajumi = [
+    {
+        "jautajums": "...",
+        "atbildes": ["A) ...", 
+                     "B) ...", 
+                     "C) ...", 
+                     "D) ..."],
+        "pareizas_atbildes": {"A", "C"}
+    }
+]
+
+T = Tests(jautajumi)
+T.pildit_testu()
